b201cb9e274dcfd6bf9217367e5514b73d50cc77
elizabethqdang <elizabethqdang@gmail.com>
2020-09-30 05:33:48 -0700

    frontend set up

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,9 +3,9 @@ import { Provider } from 'react-redux';
 import App from './app.jsx';
 
 const Root = ({ store }) => (
-  <Provider store={store}>
-    <App />
-  </Provider>
+	<Provider store={store}>
+		<App />
+	</Provider>
 );
 
 export default Root;
