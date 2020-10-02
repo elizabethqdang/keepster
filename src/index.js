4ffd9a30c5205a5a29f2c277b976dddfd8c18cfe
eqdang <elizabethqdang@gmail.com>
2020-10-02 03:44:06 -0700

    refactored api/auth related files for auth

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import './index.css';
+// import App from './components/App';
+// import * as serviceWorker from './serviceWorker';
+//Components
+import Root from './components/root';
+import configureStore from './store/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+document.addEventListener('DOMContentLoaded', () => {
+	let store;
+	if (window.currentUser) {
+		const preloadedState = { session: { currentUser: window.currentUser } };
+		store = configureStore(preloadedState);
+		delete window.currentUser;
+	} else {
+		store = configureStore();
+	}
+	const root = document.getElementById('root');
+	ReactDOM.render(<Root store={store} />, root);
+});
+
+// index.js
+//
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
