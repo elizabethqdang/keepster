06b73f4ce7e341859fb87ed6ab71118e326824c6
elizabethqdang <elizabethqdang@gmail.com>
2020-10-20 03:48:55 -0700

    updates

--- frontend/keepster.jsx
+++ frontend/keepster.jsx
@@ -5,10 +5,21 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const preloadedState = localStorage.state ?
-    JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
-
-  const root = document.getElementById('content');
-  ReactDOM.render(<Root store={store} />, root);
+	let store;
+	if (window.currentUser) {
+		const preloadedState = {
+			session: {
+				// id: window.currentUser.id,
+				currentUser: window.currentUser,
+			},
+			users: { [window.currentUser.id]: window.currentUser }
+		};
+		store = configureStore(preloadedState);
+		delete window.currentUser;
+	} else {
+		store = configureStore();
+	}
+	
+	const root = document.getElementById("content");
+	ReactDOM.render(<Root store={store} />, root);
 });
