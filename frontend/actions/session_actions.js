2f93b3bfdb6dda9c9a8db62398034b92cd170b21
elizabethqdang <elizabethqdang@gmail.com>
2020-10-25 03:56:23 -0700

    navbar styling edit

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,4 +1,4 @@
-import * as SessionAPIUtil from '../util/session_api_util';
+// import * as SessionAPIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
@@ -19,27 +19,27 @@ export const clearSessionErrors = () => ({
 	errors: []
 });
 
-export const signup = user => dispatch => (
-	SessionAPIUtil.signup(user).then(
-		user => (dispatch(receiveCurrentUser(user))),
-		errors => (dispatch(receiveSessionErrors(errors.responseJSON)))
-	));
+// export const signup = user => dispatch => (
+// 	SessionAPIUtil.signup(user).then(
+// 		user => (dispatch(receiveCurrentUser(user))),
+// 		errors => (dispatch(receiveSessionErrors(errors.responseJSON)))
+// 	));
 
-export const login = user => dispatch => (
-	SessionAPIUtil.login(user).then(
-		user => (dispatch(receiveCurrentUser(user))),
-		errors => (dispatch(receiveSessionErrors(errors.responseJSON)))
-	));
+// export const login = user => dispatch => (
+// 	SessionAPIUtil.login(user).then(
+// 		user => (dispatch(receiveCurrentUser(user))),
+// 		errors => (dispatch(receiveSessionErrors(errors.responseJSON)))
+// 	));
 
-export const logout = () => dispatch => (
-	SessionAPIUtil.logout().then(
-		user => (dispatch(receiveCurrentUser(null))),
-		errors => (dispatch(receiveSessionErrors(errors.responseJSON)))
-	));
+// export const logout = () => dispatch => (
+// 	SessionAPIUtil.logout().then(
+// 		user => (dispatch(receiveCurrentUser(null))),
+// 		errors => (dispatch(receiveSessionErrors(errors.responseJSON)))
+// 	));
 
-export const fetchCurrentUser = currentUserId => dispatch => {
-	return SessionAPIUtil.fetchCurrentUser(currentUserId).then(
-		currentUser => { dispatch(receiveCurrentUser(currentUser)) }
-		// return currentUser;
-	)
-};
+// export const fetchCurrentUser = currentUserId => dispatch => {
+// 	return SessionAPIUtil.fetchCurrentUser(currentUserId).then(
+// 		currentUser => { dispatch(receiveCurrentUser(currentUser)) }
+// 		return currentUser;
+// 	)
+// };
